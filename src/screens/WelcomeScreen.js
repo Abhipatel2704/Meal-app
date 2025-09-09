534180b46c1faaa30e01a7949aa0a4683a2e0a86
Abhipatel2704 <abhi.j.patel2704@gmail.com>
2025-09-09 17:27:43 +0530

    commiting

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -15,8 +15,8 @@ const WelcomeScreen = () => {
       <StatusBar style="light" />
 
       {/* logo with rings */}
-      <View style={tw`bg-white/20 rounded-full p-10`}>
-        <View style={tw`bg-white/20 rounded-full p-8`}>
+      <View style={[tw`bg-white/20 rounded-full`, { padding: hp(5) }]}>
+        <View style={[tw`bg-white/20 rounded-full`, { padding: hp(4.5) }]}>
           <Image
             style={[tw`rounded-full`, { width: hp(20), height: hp(20) }]}
             source={require("../../assets/welcome.jpeg")}
@@ -26,10 +26,20 @@ const WelcomeScreen = () => {
 
       {/* title and punchline */}
       <View style={tw`flex items-center space-y-2`}>
-        <Text style={tw`font-bold text-white tracking-widest text-6xl`}>
+        <Text
+          style={[
+            tw`font-bold text-white tracking-widest text-6xl`,
+            { fontSize: hp(7) },
+          ]}
+        >
           Foddy
         </Text>
-        <Text style={tw`font-medium text-white tracking-widest text-lg`}>
+        <Text
+          style={[
+            tw`font-medium text-white tracking-widest text-lg`,
+            { fontSize: hp(2) },
+          ]}
+        >
           Food is always right
         </Text>
       </View>
